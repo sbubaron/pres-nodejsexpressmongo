0cf991e3516e53843282b30c2c3d2f3771a888d9
Rich von Rauchhaupt <Rich@Snow.laptop>
2018-02-17 00:08:49 -0500

    updates

--- demos/04-express-mongo/models/quotes.js
+++ demos/04-express-mongo/models/quotes.js
@@ -1,6 +1,7 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
+//defines a simple schema for storing quotes and the character that says it
 var QuoteSchema   = new Schema({
     quote: String,
     character: String
--- demos/04-express-mongo/routes/quotes.js
+++ demos/04-express-mongo/routes/quotes.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+//the controller contains the callback functions that are passed to these routes.
 var quotesController = require('../controllers/quoteController');
 
+//cors stands for Cross-origin resource sharing
+//by default, access to resources is restricted only to requests coming from the exact same domain
+//subdomains and different ports are NOT granted access. 
 var cors = require('cors');
 
+//we can provide a list of whitelisted domains
+//or use * to allow any domain. 
 var whitelist = ['https://xyz.abc.com', 'http://localhost:4000', '*'];
 
+
+//quick check to see if the req headers origin is from an allowed domain, if it is, we let them through, if not, sorry charlie.
 var corsOptions = function (req, callback) {
     var corsOptions;
     
@@ -19,23 +27,30 @@ var corsOptions = function (req, callback) {
     callback(null, corsOptions) // callback expects two parameters: error and options
 }
 
-
-
-
+//tell our routes to check CORS status.
 router.use(cors(corsOptions));
+//certain CORS requests are considered 'complex' and require an initial OPTIONS request (called the "pre-flight request"). 
+//An example of a 'complex' CORS request is one that uses an HTTP verb other than GET/HEAD/POST (such as DELETE) or that uses custom headers. 
+//To enable pre-flighting, you must add a new OPTIONS handler for the route you want to support:
 router.options('*', cors(corsOptions));
 
+//defines a route that gets/returns all our quotes.
 router.get('/', cors(corsOptions), quotesController.findAll);
+
+//defines a route that gets returns all our quotes by character
 router.get('/by-character/:character', cors(corsOptions), quotesController.findByCharacter);  
+
+//defines a route that gets a single quote by its ID
+//the /:id represents a parameter contained in the URL
 router.get('/:id', cors(corsOptions), quotesController.findOne);
 
-//create  
+//creates a new quote   
 router.post('/', cors(corsOptions), quotesController.add);
 
-//update
+//updates/replaces an existing quote identified by the id using data in body
 router.put('/:id', cors(corsOptions), quotesController.update);
 
-//delete  
+//deletes the quote specified by the provided id
 router.delete('/:id', cors(corsOptions), quotesController.remove);
 
 module.exports = router;
